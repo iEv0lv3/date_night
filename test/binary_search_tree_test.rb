fffde293b68e86abe94b77b19f59a4cba9d05606
Jordan <jordan.wallace.williams@gmail.com>
2019-12-01 14:40:06 -0700

    Add and update binary tree search test logic

--- test/binary_search_tree_test.rb
+++ test/binary_search_tree_test.rb
@@ -2,8 +2,14 @@ require 'simplecov'
 SimpleCov.start
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/node'
+require './lib/binary_search_tree'
 require 'pry'
 
 class BinarySearchTreeTest < Minitest::Test
+  def test_binary_search_tree_exists
+    tree = BinarySearchTree.new
 
+    assert_instance_of BinarySearchTree, tree
+  end
 end
