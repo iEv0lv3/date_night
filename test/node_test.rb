cf1f1f66e661d5b47ca4434db53538fc87e6e922
Jordan <jordan.wallace.williams@gmail.com>
2019-12-01 14:38:19 -0700

    Add node class test

---
+++ test/node_test.rb
@@ -0,0 +1,11 @@
+require 'simplecov'
+SimpleCov.start
+require 'minitest/autorun'
+require 'minitest/pride'
+require '.lib/node'
+require './lib/binary_search_tree'
+require 'pry'
+
+class NodeTest < Minitest::Test
+  
+end
