c65701d69c0c7cb2e698853ab316a464392f08e4
Jordan <jordan.wallace.williams@gmail.com>
2019-12-01 20:04:17 -0700

    Add and update logic for insert method

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -4,4 +4,16 @@ class BinarySearchTree
   def initialize
     @root = nil
   end
+
+  def insert(number, data)
+    @root = Node.new(number, data) if @root.nil?
+    current_node = @root
+    if number > @root.number
+      current_node = current_node.right until current_node.right.nil?
+      current_node.right = Node.new(number, data)
+    elsif number < @root.number
+      current_node = current_node.left until current_node.left.nil?
+      current_node.left = Node.new(number, data)
+    end
+  end
 end
