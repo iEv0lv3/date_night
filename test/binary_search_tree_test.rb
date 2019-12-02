252a8dcbc2ab92b3ad7c2382d7f725c8ebad4461
Jordan <jordan.wallace.williams@gmail.com>
2019-12-01 18:11:25 -0700

    Add tests for binary tree search class, 100% coverage

--- test/binary_search_tree_test.rb
+++ test/binary_search_tree_test.rb
@@ -12,4 +12,18 @@ class BinarySearchTreeTest < Minitest::Test
 
     assert_instance_of BinarySearchTree, tree
   end
+
+  def test_binary_search_tree_has_root
+    tree = BinarySearchTree.new
+
+    assert_nil tree.root
+  end
+
+  def test_binary_search_tree_root_can_be_set
+    tree = BinarySearchTree.new
+    tree.root = Node.new(78, 'Star Wars: A New Hope')
+
+    assert_equal 78, tree.root.number
+    assert_equal 'Star Wars: A New Hope', tree.root.data
+  end
 end
